6870c217d60ec8f53b6c0560eb4fb1e08036bd3f
guptavaasu <vaasu16275@iiitd.ac.in>
2017-11-29 13:19:06 +0530

    mem

--- ArmSim.py
+++ ArmSim.py
@@ -14,6 +14,77 @@ b = [];
 opcode = [];
 cond = [];
 middle = [];
+memory = [i for i in range(2048)]
+Z = 0;
+V = 0;
+C = 0;
+N = 0;
+def decode():
+	lel = b[reg[15]]
+	condstring = ""
+	mainstring = ""
+	firstop = ""
+	secondop = ""
+	destinationreg = ""
+	num  = str(lel)
+	if (num[:4]=="0000"):
+		condstring = "EQ"
+	elif (num[:4]=="0001"):
+		condstring = "NE"
+	elif (num[:4]=="0100"):
+		condstring = "MI"
+	elif (num[:4]=="0101"):
+		condstring = "PL"
+	elif (num[:4]=="1010"):
+		condstring = "GE"
+	elif (num[:4]=="1011"):
+		condstring = "LT"
+	elif (num[:4]=="1100"):
+		condstring = "GT"
+	elif (num[:4]=="1101"):
+		condstring = "LE"
+	if(num[4:6]=="00"):
+		if(num[7:10]=="0000"):
+			mainstring = "AND"
+
+		elif(num[7:10]=="0010"):
+			mainstring = "SUB"
+		elif(num[7:10]=="0100"):
+			mainstring = "ADD"
+		elif(num[7:10]=="1010"):
+			mainstring = "CMP"
+		elif(num[7:10]=="1011"):
+			mainstring = "CMN"
+		elif(num[7:10]=="1100"):
+			mainstring = "ORR"
+		elif(num[7:10]=="1101"):
+			mainstring = "MOV"
+		elif(num[7:10]=="1111"):
+			mainstring = "MVN"
+		firstop = "R"+str(int(lel[12:16],2))
+		if(num[6]=="0"):
+			secondop = "R" + str(int(lel[20:],2))
+			print("Operation is "+mainstring+condstring+", First Operand is "+firstop+", immediate Second Operand is "+secondop)
+		else:
+			secondop = str(int(lel[20:],2))
+			print("Operation is "+mainstring+condstring+", First Operand is "+firstop+",  Second Operand is "+secondop)
+		destinationreg = "R" + str(int(lel[16:20],2))
+		print("Destination Register is "+destinationreg)
+	elif(num[4:6]=="01"):
+		firstop = "R"+str(int(lel[12:16],2))
+		secondop = "R"+str(int(lel[16:20],2))
+		if(num[11]=="0"):
+			mainstring = "STR"
+			print("Operation is "+mainstring+condstring+", First operand(base address) is "+firstop+", Source register is "+secondop)
+		elif(num[11]=="1"):
+			mainstring = "LDR"
+			print("Operation is "+mainstring+condstring+", First operand(base address) is "+firstop+", Destination register is "+secondop)
+		offset = str(int(lel[20:],2))
+		print("Offset is "+offset)
+	elif(num[4:6]=="10"):
+		mainstring = "B"
+		print("Operation is "+mainstring+condstring)
+
 
 if __name__ == '__main__':
   f = open("test.mem","r");
