a503a7076de1c7b07830756ab557ea77cc130ac3
guptavaasu <vaasu16275@iiitd.ac.in>
2017-11-29 19:59:35 +0530

    lol

--- ArmSim.py
+++ ArmSim.py
@@ -1,13 +1,3 @@
-
-#print('Fetch instruction '+in[1].lower()+'from address '+in[0].lower(),end='\n')
-#print('DECODE: Operation is '+', First Operand is '+', immediate Second Operand is '+', Destination Register is '+'.',end='\n')
-#print('Read Registers: ')
-#print(' = ')
-#print(end='\n')
-#print('EXECUTE: '+' in ',end='\n');
-#print('MEMORY: No memory operation',end='\n');
-#print('WRITEBACK: write '+' to ',end='\n');
-
 reg = [0]*16;
 global flag;
 nxt=0;
