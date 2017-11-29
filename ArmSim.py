25aa8bf9003e391d915d7b35b415a96b24ed481d
guptavaasu <vaasu16275@iiitd.ac.in>
2017-11-29 08:55:59 +0530

    lol

--- ArmSim.py
+++ ArmSim.py
@@ -8,29 +8,26 @@
 #print('MEMORY: No memory operation',end='\n');
 #print('WRITEBACK: write '+' to ',end='\n');
 
-reg = 0;
+reg = [0]*16;
 instr = [];
 b = [];
 opcode = [];
 cond = [];
 middle = [];
-pointer = 0;
-
-
-def decode():
-  print();
 
 if __name__ == '__main__':
   f = open("test.mem","r");
-  if f == null:
+  if f == None:
     print("File does not exist.")
     exit(1);
   for l in f:
     instr.append(l.replace('\n',''));
   for l in instr:
-    b.append(bin(int(l[7:],16))[2:])
+    b.append(bin(int(l[l.find(' ')+1:],16))[2:])
   for l in b:
     cond.append(l[0:4]);
     middle.append(l[4:7]);
     opcode.append(l[7:11]);
-  print(cond);
+  while(True):
+    print("Fetch instruction "+instr[reg[15]][0:instr[reg[15]].find(" ")]+" ");
+  print(instr);
